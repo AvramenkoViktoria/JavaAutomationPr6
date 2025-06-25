441c2fe9b29f9d91f6d3c611b3b75a5a6f9d5b59
Viktoria Avramenko <avramenkovictoria.1402@gmail.com>
2025-06-25 21:34:22 +0300

    Finished Practice 6

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Application entry point.
+ *
+ * <p>Made by Viktoria Avramenko.
+ * Test message.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Viktoria Avramenko!");
+    }
+}
